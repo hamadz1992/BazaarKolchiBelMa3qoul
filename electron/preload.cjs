e6907946d16a867135b4b452d40ffcef2c7343ea
hamadz1992 <34457332+hamadz1992@users.noreply.github.com>
2026-08-14 15:47:04 +0100

    ربط واجهة البرنامج بمدير الطباعة في Electron

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('desktopAPI', {
   getInfo: () => ipcRenderer.invoke('desktop:get-info'),
   getPrinters: () => ipcRenderer.invoke('desktop:printers'),
+  printHtml: (payload) => ipcRenderer.invoke('desktop:print-html', payload),
   openPath: (target) => ipcRenderer.invoke('desktop:open-path', target)
 });
